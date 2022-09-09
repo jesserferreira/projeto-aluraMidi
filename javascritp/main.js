68543bd64a1663b14b0ea35c80809e3e13f71f86
Jessér Ramiro Ferreira <97418530+jesserferreira@users.noreply.github.com>
2022-09-09 09:36:32 -0300

    Initial commit

---
+++ javascritp/main.js
@@ -0,0 +1,17 @@
+function playSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = playSom;
+    contador = contador + 1;
+    console.log(contador);
+}
+
+
+
+
+//[tecla_pom, tecla_clap, tecla_tim, tecla_puff, tecla_splash, tecla_toim, tecla_psh, tecla_tic, tecla_tom]
